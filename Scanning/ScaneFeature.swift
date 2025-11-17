c9617b55f8d1e99e5204f4ff473d98e0388aef4b
Youngmin Cho <103130581+Youngmin322@users.noreply.github.com>
2025-11-17 16:43:05 +0900

    feat:  State 정의 및 Action과 Reducer 로직 변경

--- Scanning/ScaneFeature.swift
+++ Scanning/ScaneFeature.swift
@@ -9,17 +9,23 @@ import ComposableArchitecture
 
 struct ScaneFeature: Reducer {
     struct State: Equatable {
-
+        var isScanning = false // 스캔 중인지 아닌지 확인하는 상태 변수
     }
     
     enum Action: Equatable {
         case scanButtonTapped
+        case toggleScanning
     }
     
+    // Action이 들어오면 State 변경을 담당하는 함수
     func reduce(into state: inout State, action: Action) -> Effect<Action> {
         switch action {
         case .scanButtonTapped:
-            print("스캔 버튼 눌림!")
+            return .send(.toggleScanning)
+            
+        case .toggleScanning:
+            state.isScanning.toggle()
+            print("스캔 상태: \(state.isScanning)")
             return .none
         }
     }
