460f9c4f9287f0dfc7022adb4f9430efd40bfb42
Youngmin Cho <103130581+Youngmin322@users.noreply.github.com>
2025-11-30 15:27:34 +0900

    feat: AppFeature에 CaptureFeature 통합

--- Scanning/AppFeature.swift
+++ Scanning/AppFeature.swift
@@ -12,6 +12,7 @@ struct AppFeature {
     @ObservableState
     struct State: Equatable {
         var currentScreen: Screen = .ready
+        var captureState: CaptureFeature.State?
         
         enum Screen: Equatable {
             case ready // 시작 화면
@@ -22,25 +23,38 @@ struct AppFeature {
     
     enum Action {
         case startCaptureButtonTapped // 시작 버튼 터치
-        case captureCompleted // 챕처 완료
         case processingCompleted // 처리 완료
+        case captureAction(CaptureFeature.Action)
     }
     
     var body: some Reducer<State, Action> {
-        Reduce { state, action in
-            switch action {
-            case .startCaptureButtonTapped:
-                state.currentScreen = .capturing
-                return .none
-                
-            case .captureCompleted:
-                state.currentScreen = .processing
-                return .none
-                
-            case .processingCompleted:
-                state.currentScreen = .ready
-                return .none
-            }
-        }
-    }
-}
+          Reduce { state, action in
+              switch action {
+              case .startCaptureButtonTapped:
+                  state.currentScreen = .capturing
+                  state.captureState = CaptureFeature.State()  // State 생성
+                  return .none
+                  
+              case .captureAction(.delegate(.captureCompleted)):  // 자식이 완료 알림
+                  state.currentScreen = .processing
+                  state.captureState = nil
+                  return .none
+                  
+              case .captureAction(.delegate(.captureCancelled)):  // 자식이 취소 알림
+                  state.currentScreen = .ready
+                  state.captureState = nil
+                  return .none
+                  
+              case .captureAction:  // 나머지는 자식이 처리
+                  return .none
+                  
+              case .processingCompleted:
+                  state.currentScreen = .ready
+                  return .none
+              }
+          }
+          .ifLet(\.captureState, action: \.captureAction) {  // 자식 Reducer 연결
+              CaptureFeature()
+          }
+      }
+  }
